058461090c9d36b9188a5f78ef45d8f67ea0d99b
TheLancelot <nkcubing1@gmail.com>
2025-05-21 19:54:09 +0530

    some fix

--- amul_api.py
+++ amul_api.py
@@ -62,9 +62,13 @@ def get_amul_data_urllib():
     url = "https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1&fields[lp_seller_ids]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&filters[0][original]=1&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0&cdc=1m&substore=66505ff0998183e1b1935c75"
 
     headers = {
-        "User-Agent": "Mozilla/5.0",
+    'referer': 'https://shop.amul.com/',
+    "authority": "shop.amul.com",
+    "method": "GET",
+    "scheme": "https",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Accept': 'application/json'
     }
-
     req = urllib.request.Request(url, headers=headers)
     try:
         with urllib.request.urlopen(req) as response:
@@ -72,4 +76,5 @@ def get_amul_data_urllib():
             print("Products fetched:", len(data.get("data", [])))
             return data
     except Exception as e:
-        print("urllib Failed")
+        print("Failed to fetch or parse data:", e)
+        return None
