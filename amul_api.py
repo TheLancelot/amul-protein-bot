27f32380210032defa7aa79b44b54fef762e38b6
TheLancelot <nkcubing1@gmail.com>
2025-05-21 16:55:44 +0530

    fix

--- amul_api.py
+++ amul_api.py
@@ -5,6 +5,9 @@ from selenium.webdriver.chrome.service import Service
 import json
 import time
 from config import AMUL_API, COOKIE
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def get_amul_data_api():
     url = AMUL_API
@@ -18,10 +21,16 @@ def get_amul_data_api():
 
 def get_amul_data_selenium():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
+#
+    options.add_argument("--headless=chrome")  # Use stable headless mode
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--remote-allow-origins=*")
+
     driver = webdriver.Chrome(service=Service(), options=options)
     AMUL_API="https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1&fields[lp_seller_ids]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&filters[0][original]=1&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0&cdc=1m&substore=66505ff0998183e1b1935c75"
 
@@ -29,7 +38,15 @@ def get_amul_data_selenium():
      
     driver.get(url)
     time.sleep(3)
-    response_text = driver.find_element("tag name", "pre").text if driver.find_elements("tag name", "pre") else driver.page_source
+    driver.get(url)
+    try:
+        pre_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "pre"))
+        )
+        response_text = pre_element.text
+    except:
+        response_text = driver.page_source
+    # response_text = driver.find_element("tag name", "pre").text if driver.find_elements("tag name", "pre") else driver.page_source
     
     try:
         data = json.loads(response_text)
