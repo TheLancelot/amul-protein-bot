07a69360a9b20ab833a24fe4114325b8660c8e5e
TheLancelot <nkcubing1@gmail.com>
2025-05-21 20:01:34 +0530

    more try

--- amul_api.py
+++ amul_api.py
@@ -59,6 +59,7 @@ def get_amul_data_selenium():
 import urllib.request
 
 def get_amul_data_urllib():
+
     url = "https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1&fields[lp_seller_ids]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&filters[0][original]=1&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0&cdc=1m&substore=66505ff0998183e1b1935c75"
 
     headers = {
@@ -77,4 +78,21 @@ def get_amul_data_urllib():
             return data
     except Exception as e:
         print("Failed to fetch or parse data:", e)
+        return None
+    
+
+import httpx
+
+def get_amul_data_httpx():
+    url = "https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1&fields[lp_seller_ids]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&filters[0][original]=1&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0&cdc=1m&substore=66505ff0998183e1b1935c75"
+
+    try:
+        with httpx.Client(timeout=10) as client:
+            response = client.get(url)
+            response.raise_for_status()
+            data = response.json()
+            print("Products fetched:", len(data.get("data", [])))
+            return data
+    except Exception as e:
+        print("httpx request failed:", e)
         return None
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from amul_api import get_amul_data_api, get_amul_data_selenium, get_amul_data_urllib
+from amul_api import get_amul_data_api, get_amul_data_selenium, get_amul_data_urllib,get_amul_data_httpx
 from tweet_utils import generate_amul_tweet, post_tweet, send_twilio_message
 
 if __name__ == "__main__":
@@ -14,15 +14,21 @@ if __name__ == "__main__":
             tweet_data = urllib_repsonse["data"]
         else:
             print("urllib failed")
-            selenium_data = get_amul_data_selenium()
-            if len(selenium_data["data"]) == 0:
-                raise Exception("All APIs Failed")
-            tweet_data = selenium_data["data"] if selenium_data else []
+
+            https_data= get_amul_data_httpx()
+            if len(https_data["data"])>0:
+                tweet_data = https_data["data"]
+            else:
+                print("httpx failed")
+                selenium_data = get_amul_data_selenium()
+                if len(selenium_data["data"]) == 0:
+                    raise Exception("All APIs Failed")
+                tweet_data = selenium_data["data"] if selenium_data else []
     
     tweet_text = generate_amul_tweet(tweet_data)
     if tweet_text:
         print(tweet_text)
-        post_tweet(tweet_text)
+        # post_tweet(tweet_text)
         # send_twilio_message(tweet_text)  # Uncomment to enable WhatsApp notification
         print("Successfully tweeted!")
     else:
