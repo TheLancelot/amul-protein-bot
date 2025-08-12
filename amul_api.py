5c2a666830e3ea4133444ac626be654c548b217e
Nishad Khade <77608269+TheLancelot@users.noreply.github.com>
2025-08-12 23:38:34 +0530

    Update amul_api.py

--- amul_api.py
+++ amul_api.py
@@ -70,7 +70,7 @@ def get_amul_data_api():
     # response= requests.get(url,params=params,timeout=20)
     if response.status_code == 200 and len(response.json()["data"])>0:
         print("Data fetched successfully!")
-        print(response.json())
+        # print(response.json())
         return response.json()
     else:
         print("Failed to fetch data. Status code:", response.status_code)
@@ -89,18 +89,18 @@ def get_amul_data_selenium():
     options.add_argument("--remote-allow-origins=*")
 
     driver = webdriver.Chrome(service=Service(), options=options)
-    AMUL_API="https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1&fields[lp_seller_ids]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&filters[0][original]=1&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0&cdc=1m&substore=66505ff0998183e1b1935c75"
+    AMUL_API="https://shop.amul.com/api/1/entity/ms.products?fields[name]=1&fields[brand]=1&fields[categories]=1&fields[collections]=1&fields[alias]=1&fields[sku]=1&fields[price]=1&fields[compare_price]=1&fields[original_price]=1&fields[images]=1&fields[metafields]=1&fields[discounts]=1&fields[catalog_only]=1&fields[is_catalog]=1&fields[seller]=1&fields[available]=1&fields[inventory_quantity]=1&fields[net_quantity]=1&fields[num_reviews]=1&fields[avg_rating]=1&fields[inventory_low_stock_quantity]=1&fields[inventory_allow_out_of_stock]=1&fields[default_variant]=1&fields[variants]=1&fields[lp_seller_ids]=1&filters[0][field]=categories&filters[0][value][0]=protein&filters[0][operator]=in&filters[0][original]=1&facets=true&facetgroup=default_category_facet&limit=24&total=1&start=0"
 
     url = AMUL_API
 
-    base_url="https://shop.amul.com/en/browse/protein"
-    driver.get(base_url)
-    time.sleep(3)
-    print(driver.page_source)
+    # base_url="https://shop.amul.com/en/browse/protein"
+    # driver.get(base_url)
+    # time.sleep(3)
+    # print(driver.page_source)
 
     driver.get(url)
     time.sleep(3)
-    print(driver.page_source)
+    # print(driver.page_source)
     try:
         pre_element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "pre"))
